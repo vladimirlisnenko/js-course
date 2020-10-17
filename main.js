39d8f8155ddc4c01278d53dd920a830732cd5daf
Vladimir Lisnenko <vovalisnenko@gmail.com>
2020-10-18 01:35:16 +0300

    3d lesson

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
-const numberOfFilms = prompt('Сколько фильмов вы посмотрели?');
+'use strict';
+
+let numberOfFilms;
+
+start();
+
 let personalMovieDB = {
     count: numberOfFilms,
     movies: {},
@@ -7,15 +12,55 @@ let personalMovieDB = {
     privat: false
 }
 
-for (let i = 0; i < 2; i++){
-    a = prompt('Один из последних фильмов?');
-    b = prompt('Как вы его оцените?');
-personalMovieDB.movies[a] = b;
+rememberMyFilms();
+
+personalRate();
+
+console.log(`Вы посмотрели ${numberOfFilms} фильмов`);
+
+writeYourGenres();
+
+showMyDB();
+
+function writeYourGenres(){
+    for(let i = 0; i < 3; i++){
+personalMovieDB.genres[i]=prompt(`Ваш любимый жанр № ${i+1}?`)
+    }
 }
-// a = prompt('Один из последних фильмов?');
-// b = prompt('Как вы его оцените?');
-// c = prompt('Один из последних фильмов?');
-// d = prompt('Как вы его оцените?');
-// personalMovieDB.movies[a] = b;
-// personalMovieDB.movies[c] = d;
-console.log(personalMovieDB);
+
+function start() {
+    numberOfFilms = +prompt('Сколько фильмов вы посмотрели?');
+    while (numberOfFilms == null || numberOfFilms == '' || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt('Сколько фильмов вы посмотрели?');
+    }
+}
+
+function showMyDB(){
+ if (personalMovieDB.privat == false){
+     console.log(personalMovieDB)
+ }   
+}
+
+function rememberMyFilms() {
+    for (let i = 0; i < 2; i++) {
+        let a = prompt('Один из последних фильмов?');
+        let b = prompt('Как вы его оцените?');
+        if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+            personalMovieDB.movies[a] = b;
+        } else {
+            i--
+        }
+    }
+}
+
+function personalRate() {
+    if (numberOfFilms < 10) {
+        console.log('Просмотрено довольно мало фильмов')
+    } else if (numberOfFilms < 30) {
+        console.log('Вы классический зритель')
+    } else if (numberOfFilms >= 30) {
+        console.log('Вы - киноман')
+    } else {
+        console.log('error')
+    }
+}
