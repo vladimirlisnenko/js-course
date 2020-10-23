a63b398fc7bb183d8f49587dc7655ffe60ae12b6
Vladimir Lisnenko <vovalisnenko@gmail.com>
2020-10-23 22:39:04 +0300

    HW lesson 33

--- js/script.js
+++ js/script.js
@@ -25,7 +25,7 @@ const movieDB = {
 };
 // 1) Удалить все рекламные блоки со страницы (правая часть сайта)
 
-const    img = document.querySelectorAll('.promo__adv img');
+const img = document.querySelectorAll('.promo__adv img');
 
 img.forEach(item => {
     item.remove()
@@ -43,15 +43,98 @@ promoBg.style.backgroundImage = 'url("img/bg.jpg")';
 
 // 4) Список фильмов на странице сформировать на основании данных из этого JS файла. Отсортировать их по алфавиту  
 
-const movieName = document.querySelectorAll('.promo__interactive-item');
+const interactiveList = document.querySelector('.promo__interactive-list');
+const input = document.querySelector('.adding__input');
+const deleteFilm = document.querySelectorAll('.delete');
+const checkBox = document.querySelector('[type=checkbox]');
+const form = document.querySelector('.add');
 
-movieDB.movies.sort()
-movieName.forEach((item,i) =>{
-    item.textContent = `${i+1}. `+movieDB.movies[i]
-});
+function createFilmsList(parent, arr) {
+    parent.innerHTML = '';
+    arr.sort();
+    arr.forEach((film, i) => {
+        parent.innerHTML += `<li class="promo__interactive-item">${i+1}. ${film}<div class="delete"></div></li>`;
+    });
+    document.querySelectorAll('.delete').forEach((item, i) => {
+        item.addEventListener('click', () => {
+            item.parentElement.remove();
+            arr.splice(i,1);
+            createFilmsList(parent, arr);
+        })
+    });
+}
 
 // 5) Добавить нумерацию выведенных фильмов ^
 
 // li.forEach((item,i) =>{
 //     item.textContent = `${i+1}. `+sortMovies[i]
 // });
+
+
+/* Задания на урок: Урок 33.
+
+1) Реализовать функционал, что после заполнения формы и нажатия кнопки "Подтвердить" - 
+новый фильм добавляется в список. Страница не должна перезагружаться.
+Новый фильм должен добавляться в movieDB.movies.
+Для получения доступа к значению input - обращаемся к нему как input.value;
+P.S. Здесь есть несколько вариантов решения задачи, принимается любой, но рабочий.
+
+2) Если название фильма больше, чем 21 символ - обрезать его и добавить три точки
+
+3) При клике на мусорную корзину - элемент будет удаляться из списка (сложно)
+
+4) Если в форме стоит галочка "Сделать любимым" - в консоль вывести сообщение: 
+"Добавляем любимый фильм"
+
+5) Фильмы должны быть отсортированы по алфавиту */
+
+
+//  1) Реализовать функционал, что после заполнения формы и нажатия кнопки "Подтвердить" - 
+// новый фильм добавляется в список. Страница не должна перезагружаться.
+// Новый фильм должен добавляться в movieDB.movies.
+
+// const promoInteractiveList = document.querySelector('.promo__interactive-list');
+// const btn = document.querySelector('button');
+
+
+createFilmsList(interactiveList, movieDB.movies);
+
+const addFilmByClick = (e) => {
+    e.preventDefault(0);
+    if (input.value) {
+        checkLengthFilmName();
+        movieDB.movies.push(input.value);
+        createFilmsList(interactiveList, movieDB.movies);
+        isCheckBoxChecked();
+        e.target.reset();
+    }
+};
+
+form.addEventListener('submit', addFilmByClick);
+
+
+// 2) Если название фильма больше, чем 21 символ - обрезать его и добавить три точки
+function checkLengthFilmName() {
+    if (input.value.length > 21) {
+        input.value = `${input.value.substr(0, 21)}...`
+    }
+}
+
+// 3) При клике на мусорную корзину - элемент будет удаляться из списка (сложно)
+
+
+// document.querySelectorAll('.delete').forEach((item, i) => {
+//     item.addEventListener('click', () => {
+//         item.parentElement.remove();
+//         arr.splice(i,1);
+//         createFilmsList(tag, arr);
+//     })
+// });
+
+// 4) Если в форме стоит галочка "Сделать любимым" - в консоль вывести сообщение: "Добавляем любимый фильм"
+
+function isCheckBoxChecked() {
+    if (checkBox.checked){
+        console.log("Добавляем любимый фильм")
+    }
+}
